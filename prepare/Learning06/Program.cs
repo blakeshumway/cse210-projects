9d24ea72066fdcccd603140bb80e73b74002cf9d
blakeshumway <blakeshumwayrocks@gmail.com>
2024-12-15 15:53:16 -0600

    learning 6

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        //make a list for any class that inherits from the shape class
+        List<Shape> shapes = new List<Shape>();
+
+        //make a square
+        Square square = new Square("Green", 4);
+        shapes.Add(square);
+
+        //make a rectangle
+        Rectangle rectangle = new Rectangle("Yellow", 8, 4);
+        shapes.Add(rectangle);
+
+        //make a circle
+        Circle circle = new Circle("red", 7);
+        shapes.Add(circle);
+
+
+        //go through each shape and call their methods
+        foreach (Shape s in shapes)
+        {
+            //all shapes have a GetColor() method
+            string color = s.GetColor();
+
+            //all shapes have a GetArea() method, although it is different for each
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+            Console.WriteLine();
+        }
     }
 }
---
+++ prepare/Learning06/Shape.cs
@@ -0,0 +1,22 @@
+public abstract class Shape
+{
+    private string _color;
+
+    public Shape(string color)
+    {
+        _color = color;
+    }
+
+    public string GetColor()
+    {
+        return _color;
+    }
+
+    public void SetColor(string color)
+    {
+        _color = color;
+    }
+
+    //set to abstract so other funtions call change it
+    public abstract double GetArea();
+}
